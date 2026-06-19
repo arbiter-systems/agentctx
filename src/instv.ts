bd74e4611930eb6a22cf37d2a25069ca8e4404b1
Patrick Charles <patrickrcharles@gmail.com>
2026-06-19 13:31:26 -0500

    feat(cli): register example command

--- src/instv.ts
+++ src/instv.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { createProgram } from "./cli.js";
+import { addExampleCommand } from "./example.js";
 
-await createProgram()
-  .name("instv")
-  .parseAsync();
+const program = createProgram().name("instv");
+addExampleCommand(program);
+await program.parseAsync();
